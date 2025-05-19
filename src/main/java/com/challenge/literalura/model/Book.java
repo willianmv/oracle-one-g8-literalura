3727986c7b5f388e3031e357f6ae0b85f0529b39
willianmv <willianvmarques300@gmail.com>
2025-05-19 20:32:55 -0300

    feat(entities): cria model e repository base para livro e autor

---
+++ src/main/java/com/challenge/literalura/model/Book.java
@@ -0,0 +1,57 @@
+package com.challenge.literalura.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @Column(nullable = false)
+    private String language;
+
+    @Column(nullable = false)
+    private Integer downloads;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Integer getDownloads() {
+        return downloads;
+    }
+
+    public void setDownloads(Integer downloads) {
+        this.downloads = downloads;
+    }
+}
